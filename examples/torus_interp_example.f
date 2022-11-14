f1aed209ffc95114bcbdcc30fa10bcacc0dfa88f
Manas Rachh <mrachh@flatironinstitute.org>
2022-11-14 09:30:09 -0500

    adding torus routs

---
+++ examples/torus_interp_example.f
@@ -0,0 +1,218 @@
+      implicit real *8 (a-h,o-z) 
+      real *8, allocatable :: srcvals(:,:),srccoefs(:,:)
+      real *8, allocatable :: wts(:)
+      character *100 fname
+      integer ipars(2)
+
+      real *8, allocatable :: targs(:,:)
+      integer, allocatable :: ipatch_id(:)
+
+      integer, allocatable :: norders(:),ixyzs(:),iptype(:)
+
+      real *8 xyz_out(3),xyz_in(3)
+      real *8, allocatable :: rhs(:,:),rhs_interp(:,:),rhs_ex(:,:)
+      real *8 did
+      real *8, allocatable :: errs(:)
+      real *8 dpars(2)
+      integer numit,niter
+
+      real *8, allocatable :: uvs_targ(:,:)
+      integer, allocatable :: ipatchtarg(:),ixmattarg(:)
+      real *8, allocatable :: xmattarg(:)
+
+      real *8, allocatable :: xyztarg(:,:),ftarg(:)
+      integer, allocatable :: itarg(:)
+
+      real *8 pot,potex
+      complex *16 ztmp,ima
+
+      data ima/(0.0d0,1.0d0)/
+
+
+      call prini(6,13)
+
+      done = 1
+      pi = atan(done)*4
+
+      rmajor = 2.0d0
+      rminor = 1.0d0
+
+
+      rmax = 1.0d0/2
+      call get_torus_mem(rmajor,rminor,rmax,npatches)
+      call prinf('npatches=*',npatches,1)
+
+      norder = 4 
+      npols = (norder+1)*(norder+2)/2
+
+      npts = npatches*npols
+      allocate(srcvals(12,npts),srccoefs(9,npts))
+      allocate(norders(npatches),ixyzs(npatches+1),iptype(npatches))
+
+      call get_torus_geom(rmajor,rminor,rmax,norder,npatches,
+     1   npts,norders,ixyzs,iptype,srcvals,srccoefs)
+      call prinf('npatches=*',npatches,1)
+      call prinf('npts=*',npts,1)
+
+      uu = hkrand(0)*2*pi
+      vv = hkrand(0)*2*pi
+      rrout = (rmajor+rminor)*3.0d0 
+      xyz_out(1) = rrout*sin(uu)*cos(vv)
+      xyz_out(2) = rrout*sin(uu)*sin(vv)
+      xyz_out(3) = rrout*cos(uu)
+
+
+      
+      uu = hkrand(0)*2*pi
+      vv = hkrand(0)*pi/4
+      
+      xyz_in(3) = rminor*sin(vv)
+      rin = (rmajor + rminor*cos(vv))*0.5d0
+      xyz_in(1) = rin*cos(uu)
+      xyz_in(2) = rin*sin(uu)
+
+      xyz_in(1) = rmajor+rminor+0.01d0
+      xyz_in(2) = 0.032d0
+      xyz_in(3) = -0.011d0
+      
+      allocate(wts(npts))
+      call get_qwts(npatches,norders,ixyzs,iptype,npts,srcvals,wts)
+      nd = 2
+      call prin2('wts=*',wts,12)
+      ra = sum(wts)
+      call prin2('area of torus=*',ra,1)
+!
+!
+!  Create two test functions due to point source inside and outside
+!
+      allocate(rhs(2,npts))
+
+      ra = 0
+
+      do i=1,npts
+        call l3d_slp(xyz_in,3,srcvals(1,i),0,dpars,0,zpars,0,ipars,
+     1     rhs(1,i))
+        call l3d_slp(xyz_out,3,srcvals(1,i),0,dpars,0,zpars,0,ipars,
+     1     rhs(2,i))
+        ra = ra + (rhs(1,i)**2 + rhs(2,i)**2)*wts(i)
+      enddo
+      ra = sqrt(ra)
+
+      call prin2('ra=*',ra,1)
+
+
+
+      ntarg = 1
+      allocate(targs(3,ntarg),rhs_ex(nd,ntarg),rhs_interp(nd,ntarg))
+      do i=1,ntarg
+        uu = hkrand(0)*2*pi
+        vv = hkrand(0)*2*pi
+
+        print *, "uu=",uu
+        print *, "vv=",vv
+        targs(1,i) = (rmajor+rminor*cos(vv))*cos(uu) 
+        targs(2,i) = (rmajor+rminor*cos(vv))*sin(uu) 
+        targs(3,i) = rminor*sin(vv)
+        call l3d_slp(xyz_in,3,targs(1,i),0,dpars,0,zpars,0,ipars,
+     1     rhs_ex(1,i))
+        call l3d_slp(xyz_out,3,targs(1,i),0,dpars,0,zpars,0,ipars,
+     1     rhs_ex(2,i))
+      enddo
+
+      call torus_interp(nd,rmajor,rminor,rmax,ntarg,targs,npatches,
+     1  norders,ixyzs,iptype,npts,rhs,rhs_interp)
+      erra = 0
+      do i=1,ntarg
+        do idim=1,nd
+          erra = erra + (rhs_interp(idim,i) - rhs_ex(idim,i))**2
+        enddo
+      enddo
+
+      erra = sqrt(erra/ntarg)
+      erra = erra
+      call prin2('relative l2 error in interpolated value at targets=*',
+     1   erra,1)
+      return
+
+      allocate(ipatchtarg(ntarg),uvs_targ(2,ntarg))
+      call ellipsoid_local_coord_targ(a,b,c,rmax,ifc,ntarg,targs,
+     1   npatches,norders,ixyzs,iptype,npts,ipatchtarg,uvs_targ)
+      
+
+      lmem = 0
+
+      call get_surf_interp_mat_targ_mem(npatches,ixyzs,ntarg,
+     1  ipatchtarg,lmem)
+      call prinf('lmem =*',lmem,1)
+      call prinf('expected lmem=*',ntarg*npols,1)
+
+      allocate(ixmattarg(ntarg+1),xmattarg(lmem))
+      call get_surf_interp_mat_targ(npatches,norders,ixyzs,iptype,npts,
+     1 ntarg,ipatchtarg,uvs_targ,lmem,xmattarg,ixmattarg)
+
+c
+c  check interpolation error using xmattarg
+c
+c
+      rhs_interp = 0
+
+      do i=1,ntarg
+        ipatch = ipatchtarg(i)
+        istart = ixyzs(ipatch)
+        istart2 = ixmattarg(i)
+        npols_use = ixyzs(ipatch+1) - ixyzs(ipatch) 
+        do l=1,npols
+          do idim = 1,nd
+            rhs_interp(idim,i) = rhs_interp(idim,i) +
+     1         xmattarg(istart2+l-1)*rhs(idim,istart+l-1)
+          enddo
+        enddo
+      enddo
+
+
+      erra = 0
+      do i=1,ntarg
+        do idim=1,nd
+          erra = erra + (rhs_interp(idim,i) - rhs_ex(idim,i))**2
+        enddo
+      enddo
+
+      erra = sqrt(erra/ntarg)
+      erra = erra
+      call prin2('relative l2 error in interpolated value at targets=*',
+     1   erra,1)
+
+      
+      ntarg = 1000
+      allocate(xyztarg(3,ntarg))
+
+      allocate(itarg(ntarg),ftarg(ntarg))
+
+      xmax = 2*a
+      xmin = -2*a
+      ymax = 2*b
+      ymin = -2*b
+      zmax = 2*c
+      zmin = -2*c
+
+      do i=1,ntarg
+        xyztarg(1,i) = hkrand(0)*(xmax-xmin) + xmin
+        xyztarg(2,i) = hkrand(0)*(ymax-ymin) + ymin
+        xyztarg(3,i) = hkrand(0)*(zmax-zmin) + zmin
+        call check_ellipsoid_interior(a,b,c,xyztarg(1,i),itarg(i))
+        ftarg(i) = itarg(i) + 0.0d0
+      enddo
+
+      call surf_vtk_plot(npatches,norders,ixyzs,iptype,npts,srccoefs,
+     1   srcvals,'ellipsoid.vtk','a')
+      call vtk_scatter_plot_scalar(ntarg,xyztarg,
+     1   ftarg,'ellip_pts.vtk','a')
+      
+
+
+      
+
+      stop
+      end
+
+
---
+++ src/torus_routs.f90
@@ -0,0 +1,391 @@
+      subroutine get_torus_mem(rmajor,rminor,rmax,npatches)
+!
+!  This subroutine estimates the number of patches required in
+!  the discretization of a standard torus defined via the major and
+!  minor axes given by rmajor=r1, and rminor=r2. The parametrization
+!  of the surface is given by
+!
+!  X(u,v) = (r1 + r2*cos(v))*cos(u)
+!  Y(u,v) = (r1 + r2*cos(v))*sin(u)
+!  Z(u,v) = r2*sin(v)
+!
+!  Input arguments:
+!    - rmajor: real *8
+!        major axis of the torus
+!    - rminor: real *8
+!        minor axis of the torus
+!    - rmax: real *8
+!        max size of patch (this is just an estimate, rmax 
+!        is used as a heuristic to divide the u axis in 2*pi*(r1+r2)/rmax
+!        chunks, and the v axis into 2*pi*r2/rmax chunks
+!
+!  Output arguments:
+!    - npatches: integer
+!        number of patches used in the discretization
+!
+!
+      implicit none
+      real *8, intent(in) :: rmajor,rminor,rmax
+      integer, intent(out) :: npatches
+
+      real *8 pi,done
+      integer nu,nv
+
+
+      done = 1.0d0
+      pi = atan(done)*4.0d0
+
+      nu = max(ceiling(2*pi*(rmajor+rminor)/rmax),3)
+      nv = max(ceiling(2*pi*rminor/rmax),3)
+      npatches = 2*nu*nv
+
+      return
+      end
+
+
+
+      subroutine get_torus_geom(rmajor,rminor,rmax,norder,npatches, &
+        npts,norders,ixyzs,iptype,srcvals,srccoefs)
+!
+!  This subroutine estimates the number of patches required in
+!  the discretization of a standard torus defined via the major and
+!  minor axes given by rmajor=r1, and rminor=r2. 
+!  The parametrization of the surface is given by
+!
+!  X(u,v) = (r1 + r2*cos(v))*cos(u)
+!  Y(u,v) = (r1 + r2*cos(v))*sin(u)
+!  Z(u,v) = r2*sin(v)
+!
+!  Input arguments:
+!    - rmajor: real *8
+!        major axis of the torus
+!    - rminor: real *8
+!        minor axis of the torus
+!    - rmax: real *8
+!        max size of patch (this is just an estimate, rmax 
+!        is used as a heuristic to divide the u axis in 2*pi*(r1+r2)/rmax
+!        chunks, and the v axis into 2*pi*r2/rmax chunks
+!    - norder:
+!        order of discretization
+!    - npatches: integer
+!        number of patches used in the discretization
+!    - npts: integer
+!        number of points in the discretization = 
+!          npatches*(norder+1)*(norder+2)/2
+!
+!  Output arguments:
+!    - norders: integer(npatches)
+!        discretization order of patches
+!    - ixyzs: integer(npatches+1)
+!        starting location of points on patch i
+!    - iptype: integer(npatches)
+!        type of patch
+!        iptype = 1, triangle discretized using RV nodes
+!    - srccoefs: double precision (9,npts)
+!        koornwinder expansion coefs of geometry info
+!    - srcvals: double precision (12,npts)
+!        xyz, dxyz/du,dxyz/dv, normals at all nodes
+!
+!
+!
+      implicit real *8 (a-h,o-z)
+      real *8, intent(in) :: rmajor,rminor,rmax
+      integer, intent(in) :: npatches,npts,norder
+      integer, intent(out) :: norders(npatches),ixyzs(npatches+1)
+      integer, intent(out) :: iptype(npatches)
+      real *8, intent(out) :: srcvals(12,npts),srccoefs(9,npts)
+      
+      real *8 pi,done
+      integer nu,nv
+      real *8 umin,umax,vmin,vmax
+      real *8, allocatable, target :: triaskel(:,:,:)
+      integer nover
+      real *8, target :: p1(3),p2(3),p3(1),p4(1)
+      real *8, pointer :: ptr1,ptr2,ptr3,ptr4
+
+      real *8, allocatable :: uvs(:,:),wts(:),umatr(:,:),vmatr(:,:)
+
+      procedure (), pointer :: xtri_geometry
+
+      external xtri_wtorus_eval
+      
+
+
+      done = 1.0d0
+      pi = atan(done)*4.0d0
+
+      nu = max(ceiling(2*pi*(rmajor+rminor)/rmax),3)
+      nv = max(ceiling(2*pi*rminor/rmax),3)
+      npols = (norder+1)*(norder+2)/2
+      allocate(uvs(2,npols),wts(npols),umatr(npols,npols))
+      allocate(vmatr(npols,npols))
+
+      call vioreanu_simplex_quad(norder,npols,uvs,umatr,vmatr,wts)
+
+      umin = 0
+      umax = 2*pi
+      vmin = 0
+      vmax = 2*pi
+
+      allocate(triaskel(3,3,npatches))
+      nover = 0
+      npatches0 = 0 
+      call xtri_rectmesh_ani(umin,umax,vmin,vmax,nu,nv,nover,npatches, &
+        npatches0,triaskel)
+      
+      p1(1) = rmajor
+      p1(2) = rminor
+      p1(3) = 0.0d0
+      
+      p2(1) = 1.0d0
+      p2(2) = 1.0d0
+      p2(3) = 1.0d0
+
+      p4(1) = 0.0d0
+      ptr1 => triaskel(1,1,1)
+      ptr2 => p1(1)
+      ptr3 => p2(1)
+      ptr4 => p4(1)
+
+      xtri_geometry => xtri_wtorus_eval
+      call getgeominfo(npatches,xtri_geometry,ptr1,ptr2,ptr3,ptr4, &
+        npols,uvs,umatr,srcvals,srccoefs)
+      
+      do i=1,npatches
+        norders(i) = norder
+        ixyzs(i) = (i-1)*npols + 1
+        iptype(i) = 1
+      enddo
+      ixyzs(npatches+1) = npts+1
+
+
+      return
+      end
+
+!
+!
+!
+!
+!
+      subroutine torus_interp(nd,rmajor,rminor,rmax,ntarg,targs,npatches, &
+        norders,ixyzs,iptype,npts,u,uinterp)
+!
+!  This function interpolated a given function on surface 
+!  to a collection of targets on a triangulated torus with major
+!  and minor axis rmajor,rminor, maximum patch size rmax, at a collection
+!  of user specified targets on the ellipsoid provided in their cartesian 
+!  coordinates.
+!
+!  The parametrization of the surface is given by
+!
+!  X(u,v) = (r1 + r2*cos(v))*cos(u)
+!  Y(u,v) = (r1 + r2*cos(v))*sin(u)
+!  Z(u,v) = r2*sin(v)
+!
+!
+!
+!  Input arguments:
+!    - nd: integer
+!       number of functions
+!    - rmajor: real *8
+!       major axis of torus 
+!    - rminor: real *8
+!       minor axis of torus 
+!    - rmax: real *8
+!        maximum patch size
+!    - ntarg: integer
+!        number of targets
+!    - targs: real *8 (3,ntarg)
+!        xyz coordinates of the targets
+!    - npatches: integer
+!        number of patches in the discretization can be computed
+!        by a call to get_ellipsoid_mem
+!    - norders: integer(npatches)
+!        discretization order of patches
+!    - ixyzs: integer(npatches+1)
+!        starting location of points on patch i
+!    - iptype: integer(npatches)
+!        type of patch
+!        iptype = 1, triangle discretized using RV nodes
+!    - npts: integer
+!        number of points in the discretization = 
+!          npatches*(norder+1)*(norder+2)/2
+!    - u: real *8 (nd,npts)
+!        function values on surface
+!
+!
+!  Output arguments:
+!    - uinterp: real *8 (nd,ntarg)
+!        interpolated function values on surface
+!
+
+      implicit real *8 (a-h,o-z)
+      real *8, intent(in) :: rmajor,rminor,rmax
+      integer, intent(in) :: ntarg,nd,npts
+      real *8, intent(in) :: targs(3,ntarg)
+      integer, intent(in) :: npatches,norders(npatches),ixyzs(npatches+1)
+      integer, intent(in) :: iptype(npatches)
+      real *8, intent(in) :: u(nd,npts)
+      real *8, intent(out) :: uinterp(nd,ntarg)
+
+      integer, allocatable :: ipatchtarg(:)
+      real *8, allocatable :: uvs_targ(:,:)
+      real *8, allocatable :: ucoefs(:,:),pols(:)
+
+      real *8 pi,done
+      integer nu,nv
+
+      allocate(ipatchtarg(ntarg),uvs_targ(2,ntarg))
+      call torus_local_coord_targ(rmajor,rminor,rmax,ntarg,targs, &
+        npatches,norders,ixyzs,iptype,npts,ipatchtarg,uvs_targ)
+      allocate(ucoefs(nd,npts))
+
+      call surf_vals_to_coefs(nd,npatches,norders,ixyzs,iptype,npts,&
+        u,ucoefs)
+
+      nordermax = maxval(norders(1:npatches))
+      npmax = (nordermax+1)*(nordermax+2)/2
+      allocate(pols(npmax))
+
+      incx = 1
+      incy = 1
+
+!$OMP PARALLEL DO DEFAULT(SHARED) PRIVATE(i,norder,npols,ii,pols)
+      do i=1,ntarg
+        norder = norders(ipatchtarg(i))
+        npols = (norder+1)*(norder+2)/2
+        call koorn_pols(uvs_targ(1,i),norder,npols,pols)
+        ii = ixyzs(ipatchtarg(i))
+        
+        call dgemv_guru('n',nd,npols,alpha,ucoefs(1,ii),nd,pols,incx,beta, &
+          uinterp(1,i),incy)
+
+      enddo
+!$OMP END PARALLEL DO      
+
+      return
+      end
+
+!
+!
+!
+!
+!
+
+      subroutine torus_local_coord_targ(rmajor,rminor,rmax,ntarg,targs, &
+        npatches,norders,ixyzs,iptype,npts,ipatchtarg,uvs_targ)
+      
+!
+!  This function extracts the local patch id and uv coordinates
+!  for a collection of targets on a triangulated torus with major
+!  and minor axis rmajor,rminor, maximum patch size rmax, at a collection
+!  of user specified targets on the ellipsoid provided in their cartesian 
+!  coordinates.
+!
+!  The parametrization of the surface is given by
+!
+!  X(u,v) = (r1 + r2*cos(v))*cos(u)
+!  Y(u,v) = (r1 + r2*cos(v))*sin(u)
+!  Z(u,v) = r2*sin(v)
+!
+!
+!
+!  Input arguments:
+!    - rmajor: real *8
+!       major axis of torus 
+!    - rminor: real *8
+!       minor axis of torus 
+!    - rmax: real *8
+!        maximum patch size
+!    - ntarg: integer
+!        number of targets
+!    - targs: real *8 (3,ntarg)
+!        xyz coordinates of the targets
+!    - npatches: integer
+!        number of patches in the discretization can be computed
+!        by a call to get_ellipsoid_mem
+!    - norders: integer(npatches)
+!        discretization order of patches
+!    - ixyzs: integer(npatches+1)
+!        starting location of points on patch i
+!    - iptype: integer(npatches)
+!        type of patch
+!        iptype = 1, triangle discretized using RV nodes
+!    - npts: integer
+!        number of points in the discretization = 
+!          npatches*(norder+1)*(norder+2)/2
+!
+!  Output arguments:
+!    - ipatchtarg: integer(ntarg)
+!        ipatchtarg(i) is the patch on which target i is located
+!    - uvs_targ: real *8 (2,ntarg)
+!        local uv coordinates on patch
+!
+
+      implicit real *8 (a-h,o-z)
+      real *8, intent(in) :: rmajor,rminor,rmax
+      integer, intent(in) :: ntarg,npts
+      real *8, intent(in) :: targs(3,ntarg)
+      integer, intent(in) :: npatches,norders(npatches),ixyzs(npatches+1)
+      integer, intent(in) :: iptype(npatches)
+      integer, intent(out) :: ipatchtarg(ntarg)
+      real *8, intent(out) :: uvs_targ(2,ntarg)
+
+      real *8 pi,done
+      integer nu,nv
+
+
+      done = 1.0d0
+      pi = atan(done)*4.0d0
+
+      nu = max(ceiling(2*pi*(rmajor+rminor)/rmax),3)
+      nv = max(ceiling(2*pi*rminor/rmax),3)
+      
+      hu = 2*pi/(nu+0.0d0)
+      hv = 2*pi/(nv+0.0d0)
+
+      print *, "hu,hv=",hu,hv
+
+!$OMP PARALLEL DO DEFAULT(SHARED) PRIVATE(i,cv,sv,v,cu,su,u) &
+!$OMP PRIVATE(iu,iv,u0,v0,uuse,vuse)
+      do i=1,ntarg
+        cv = sqrt(targs(1,i)**2 + targs(2,i)**2) - rmajor
+        sv = targs(3,i)
+        v = atan2(sv,cv)
+        if(v.lt.0) v = v + 2*pi
+        cu = targs(1,i)
+        su = targs(2,i)
+        u = atan2(su,cu)
+        if(u.lt.0) u = u + 2*pi
+
+        print *, "u=",u
+        print *, "v=",v
+
+        iu = ceiling(u/hu)
+        iv = ceiling(v/hv)
+        if(iu.eq.0) iu = 1
+        if(iv.eq.0) iv = 1
+
+        print *, iu,iv,nu,nv
+
+        u0 = (iu-1)*hu
+        v0 = (iv-1)*hv
+        uuse = (u-u0)/hu
+        vuse = (v-v0)/hv
+
+        print *, uuse,vuse
+
+        if(uuse+vuse.le.1) then
+          ipatchtarg(i) = (iu-1)*nv*2 + 2*(iv-1) + 1
+          uvs_targ(1,i) = uuse
+          uvs_targ(2,i) = vuse
+        else
+          ipatchtarg(i) = (iu-1)*nv*2 + 2*(iv-1) + 2
+          uvs_targ(1,i) = 1.0d0-uuse
+          uvs_targ(2,i) = 1.0d0-vuse
+        endif
+      enddo
+!$OMP END PARALLEL DO      
+
+      return
+      end
